909a15f8be386c93e4003ea350d355440e9e501d
Ramdhan Connelo <37245888+whankz27@users.noreply.github.com>
2023-12-14 16:58:51 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import Boot from './src/Boot';
+
+const App = () => {
+  return <Boot />;
+};
+
+export default App;
